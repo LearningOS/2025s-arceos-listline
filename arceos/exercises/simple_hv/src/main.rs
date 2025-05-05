fbf5d9ed6df8412b7fcc507184f4625803b2daed
zuqis <zuqis1273@gmail.com>
2025-05-05 16:06:45 +0800

    feat:h_trap

--- arceos/exercises/simple_hv/src/main.rs
+++ arceos/exercises/simple_hv/src/main.rs
@@ -117,7 +117,7 @@ fn vmexit_handler(ctx: &mut VmCpuRegisters) -> bool {
                 stval::read(),
                 ctx.guest_regs.sepc
             );
-            ctx.guest_regs.gprs.set_reg(A0, 0x1234);
+            ctx.guest_regs.gprs.set_reg(A1, 0x1234);
             ctx.guest_regs.sepc += 4;
             return false;
         },
